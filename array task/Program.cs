481c001d9fb09fdae826abc560c2a0f2a6e3f5ae
Tacəddin Əsədzadə <tcddnesedzade@fmail.com>
2024-10-07 02:02:47 +0400

    newTask

---
+++ array task/Program.cs
@@ -0,0 +1,68 @@
+﻿//task1 
+
+//int[] array = { 7, 14, 41, 93, 198, 50, 90 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    for (int j = 2; j < array[i]; j++)
+//    {
+//        if (array[i] % j == 0)
+//        {
+//            Console.WriteLine(array[i]);
+//            break;
+//        }
+//    }
+//}
+
+
+//task2
+
+//int[] array = { 12, 78, 13, 100, 76, 90 };
+//int[] copyArray = { 13, 56, 79, 91, 34, 76, 90 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    for (int j = 0; j < copyArray.Length; j++)
+//    {
+//        if (array[i] == copyArray[j])
+//        {
+//            Console.WriteLine(array[i]);
+
+//        }
+//    }
+//}
+
+
+
+
+//task3
+
+//int max = 0, min = 0;
+//int[] array = { 7, 54, 41, 93, 198, 50, 90 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] > array[max])
+//    {
+//        max = i;
+//    }
+//    if (array[i] < array[min])
+//    {
+//        min = i;
+//    }
+//}
+////Console.WriteLine(max);
+////Console.WriteLine(min);
+//Console.WriteLine($"En kiçik deyerin indeksi: {min}, En boyuk deyerin indeksi: {max}");
+
+
+//task4
+//Console.WriteLine("eded daxil edin");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+
+//while (num > 0)
+//{
+//    sum += num % 10;
+//    num = num / 10;
+//}
+//Console.WriteLine(sum);
+
+
